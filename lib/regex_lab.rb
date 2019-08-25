ac6d42936a2886eadef7b59f413c0f0e2d494f58
Eliza Munroe <era3munroe@gmail.com>
2019-08-25 12:05:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\A[aeiouAEIOU]/) !=nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\d{10}/) 
+    true
+  else 
+    false
+  end
 end
